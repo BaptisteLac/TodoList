dbaeff54a1137ee1770ed8c567a63a6aa9837010
BaptisteLac <baptiste.lacoste0@gmail.com>
2023-03-07 09:25:01 +0100

    Add CSS and icon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, {useState} from 'react';
 import './App.css';
 import "./index.css"; 
-import { Container, Text, Button, Tasks, TaskCount, TaskList } from './styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Container, Text, Button, Tasks, TaskCount, TaskList, InputContainer, Title, CheckboxContainer } from './styled-components';
 
 
 function App() {
   const [input, setInput] = useState("");
   const [completedTaskCount, setCompletedTaskCount] = useState(0);
   const [todoList, setTodoList] = useState([]);
-const handleClick = () => {
-    const id = todoList.length + 1;
-    setTodoList((prev) => [
-      ...prev,
-      {
-        id: id,
-        task: input,
-        complete: false,
-      }
-    ]);
-    setInput("");
-  };
+
+
+  const handleClick = () => {
+      const id = todoList.length + 1;
+      setTodoList((prev) => [
+        ...prev,
+        {
+          id: id,
+          task: input,
+          complete: false,
+        }
+      ]);
+      setInput("");
+    };
+
+
   const handleComplete = (id) => {
     let list = todoList.map((task) => {
       let item = {};
@@ -38,12 +44,38 @@ return item;
     });
     setTodoList(list);
   };
+
+
+  const Checkbox = () => {
+    const [checked, setChecked] = useState(false);
+    const onChange = (event) => {
+      setChecked(event.target.checked)
+    }
+
+    return (
+      <CheckboxContainer>
+        <input type="checkbox" checked={checked} onChange={onChange}/>
+        {checked && (
+          <svg data-src="https://s2.svgbox.net/materialui.svg?ic=check" width="32" height="32" color="#000"></svg>
+        )}
+      </CheckboxContainer>
+    )
+  }
+
+  const ClearTask = () => {
+    return (
+      <FontAwesomeIcon icon={faTrash} />
+    )
+  }
+
+
 return (
     <Container>
-      <div>
-          <h2>Todo List</h2>
-          <Text value={input} onInput={(e) =>setInput(e.target.value)} />
-          <Button onClick={() => handleClick()}>Add</Button>
+          <Title>What I need to do ?</Title>
+          <InputContainer>
+            <Text value={input} onInput={(e) =>setInput(e.target.value)} placeholder={'New task'}/>
+            <Button onClick={() => handleClick()}>Add</Button>
+          </InputContainer>
         <Tasks>
           <TaskCount>
             <b>Pending Tasks</b> {todoList.length - completedTaskCount}
@@ -60,18 +92,15 @@ return (
                   complete = {todo.complete}
                   id={todo.id}
                   onClick={() => handleComplete(todo.id)}
-                  style={{
-                    listStyle: "none",
-                    textDecoration: todo.complete && "line-through",
-                  }}
                 >
+                  <Checkbox/>
                   {todo.task}
+                  <ClearTask/>
                 </TaskList>
               );
             })}
           </ul>
         </div>
-      </div>
     </Container>
   );
 };
--- src/styled-components.js
+++ src/styled-components.js
@@ -3,37 +3,88 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     align-items: center;
+    justify-content: space-around;
     flex-direction: column;
+    background-color: blue;
+    gap: 0.5rem;
+`
+
+export const Title = styled.h2`
+    color: white;
+`
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 20rem;
+    height: 2rem;
+    background-color: yellow;
 `
 
 export const Button = styled.div`
-    display:inline-block;
-    flex: 1;
-    border: none;
-    background-color: teal;
-    color: white;
-    height: 30px;
-    width: 50px;
-    border-radius: 2px;
+    padding: 0.6em 1.2em;
+    border-radius: 0.375em;
     cursor: pointer;
+    color: white;
+    font-weight: bold;
+    background-image: linear-gradient(to right, #44A08D 0%, #093637  51%, #44A08D  100%); 
+    background-size: 200% auto;
+    transition: 0.5s;
+    text-transform: uppercase;
+
+    :hover {
+        background-position: right center;
+    }
 `
 
 export const Text = styled.input`
-    border: 2px solid #000;
-    width: 200px;
-    padding: 5px;
-    border-radius: 2px;
-    margin: 5px;
+    padding: 0.7em 2em;
+    border-radius: 0.375em;
 `
 
 export const TaskCount = styled.span`
-  margin: 10px;
+    color: white;
+    font-weight: bold;
+    background-color: pink;
 `
 
 export const Tasks = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 20rem;
+    height: 2rem;
+    background-color: green;
 `
 
 export const TaskList = styled.li`
-    listStyle: "none";
-    text-decoration: "line-through";
+    color: white;
+    font-weight: bold;
+    text-transform: capitalize;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 20rem;
+    height: 2rem;
+    background-color: orange;
+`
+
+export const CheckboxContainer = styled.div`
+    border: 2px solid white;
+    border-radius: 4px;
+    background-color: black;
+    width: 16px;
+    height: 16px;
+    position: relative;
+    color: black;
+
+        input {
+            opacity: 0;
+            position: absolute;
+            inset: 0;
+        }
 `
